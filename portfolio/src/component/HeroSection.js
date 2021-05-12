31b2e557278e5d849686ea91d1274a94c484589f
SujaniThuthilochana <185026a@uom.lk>
2021-05-13 00:20:44 +0530

    Implement: Home page

---
+++ portfolio/src/component/HeroSection.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Button } from './Button';
+import './HeroSection.css';
+import video from '../videos/video-1.mp4'
+
+function HeroSection() {
+    return (
+        <div className='hero-container'>
+            <video src={video} autoPlay loop muted/>
+            {/* <img src={video}/> */}
+            <h1>ADVENTURE AWAIT</h1>
+            <p>What Are You Waiting For?</p>
+            <div className='hero-btns'>
+                <Button
+                className='btns'
+                buttonStyle='btn--outline'
+                buttonSize='btn--large'
+                >
+Get Started<i className='far fa-play-circle'/>
+                </Button>
+                <Button
+                className='btns'
+                buttonStyle='btn--primary'
+                buttonSize='btn--large'
+                >
+Watch Trailer
+                </Button>
+            </div>
+        </div>
+    )
+}
+
+export default HeroSection;
